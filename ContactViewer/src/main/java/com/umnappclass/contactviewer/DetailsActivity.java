8c959e80cb2709fe27b654443256b66239c4178e
Bret Hassler <bret.hassler@gmail.com>
2014-02-20 20:54:24 -0600

    Implement start call, send text, and send email buttons
    The 3 buttons at the bottom of the screen will now bring up the email,
    phone, or ams clients and apply the phone number/email address
    appropriately.

--- ContactViewer/src/main/java/com/umnappclass/contactviewer/DetailsActivity.java
+++ ContactViewer/src/main/java/com/umnappclass/contactviewer/DetailsActivity.java
@@ -2,9 +2,11 @@ package com.umnappclass.contactviewer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  *
@@ -16,19 +18,18 @@ public class DetailsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail);
 
+        // After pressing the button to go back to contacts,
+        // destroy the activity in order to return to the contacts list
         Button goToContactsButton = (Button)findViewById(R.id.go_to_contacts);
-
         goToContactsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
-        
 
-
+        // Go to the edit contact screen after the pressing Edit
         Button editContactButton = (Button)findViewById(R.id.edit_contact);
-
         editContactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +37,53 @@ public class DetailsActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        // Bring up the phone client with the contact's phone number after pressing the call button
+        Button callButton = (Button)findViewById(R.id.call_button);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView phoneTextView = (TextView)findViewById(R.id.details_phone);
+                String phoneNumber = phoneTextView.getText().toString();
+                phoneNumber = phoneNumber.replaceAll("[\\s\\-()]", "").trim();
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                startActivity(intent);
+            }
+        });
+
+        // Bring up the sms client with the contact's phone number after pressing the send text button
+        Button smsButton = (Button)findViewById(R.id.send_text_button);
+        smsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView phoneTextView = (TextView)findViewById(R.id.details_phone);
+                String phoneNumber = phoneTextView.getText().toString().trim();
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("sms:" + phoneNumber));
+                startActivity(intent);
+            }
+        });
+
+        // Bring up the email client with the contact's email after pressing the send email button
+        Button emailButton = (Button)findViewById(R.id.send_email_button);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView emailTextView = (TextView)findViewById(R.id.details_email);
+                String[] email = {emailTextView.getText().toString()};
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, email);
+                startActivity(intent);
+            }
+        });
     }
+
+
 
 
 }
